d411f71f74007fb0fd0d63cc319c780620cfb0d4
jadeprideaux <jadeprideaux7@gmail.com>
2025-05-02 11:26:06 +0100

    read some table data, but still having issues

---
+++ MarioKartComboApp.Server/Scraper/functions/extract_data.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+
+
+def extract_data(html):
+    # Parse HTML data and find the tables
+    tables = select_tables(html)
+    # Iterate through tables
+    for table in tables:
+        process_table_data(table)
+    # Store Extracted Data
+
+
+def select_tables(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    print(f"Page title: {soup.title.string}")
+
+    # Select all the necessary tables by checking the class
+    tables = soup.find_all('table', class_='sortable scrollable')
+
+    # Check that there are 4 tables (drivers, bodies, tires and gliders)
+    if len(tables) != 4:
+        print(f"Warning: Expected 4 tables, but found {len(tables)}.")
+    else:
+        print(f"Found {len(tables)} tables, as expected.")
+
+    return tables
+
+
+def process_table_data(table):
+    # Get and Print table title
+    title = table.tbody.tr.th.big.big.string
+    print(f"Table: {title}")
+
+    # Get all rows in the tbody
+    rows = table.tbody.find_all('tr')
+    
+
+'''
+{'Glider': [],
+ 'Speed': ['Water(SW)',
+           'Air(SA)',
+           'Anti-Gravity(SG)',
+           'Ground(TL)'],
+ 'Acceleration(AC)': [],
+ 'Weight(WG)': [],
+ 'Handling': ['Ground(TL)',
+              'Water(TW)',
+              'Air(TA)',
+              'Anti-Gravity(TG)'],
+ '(Off-Road) Traction(OF)': [],
+ 'Mini-Turbo(MT)': [],
+ 'Invincibility(IV)': [],
+ 'On-Road Traction(ON)1': []
+ }
+'''
--- MarioKartComboApp.Server/Scraper/mkscraper.py
+++ MarioKartComboApp.Server/Scraper/mkscraper.py
@@ -1,4 +1,10 @@
-﻿from functions.load_page import load_page
+﻿from functions.get_page import get_page
+from functions.extract_data import extract_data
 
 url = "https://www.mariowiki.com/Mario_Kart_8_Deluxe_in-game_statistics"
-html = load_page(url)
+
+# Get the HTML of the webpage
+html = get_page(url)
+
+# Extract Data from the HTML page
+extract_data(html)
